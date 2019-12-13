040069a8060cb8e78fbc973732f9bf9fe44b8228
SLSRA-1\VEX <slylock2@mac.com>
2019-12-12 20:25:16 -0600

    troubleshooting

--- DelmarBotsFRC/solenoids/src/main/cpp/Robot.cpp
+++ DelmarBotsFRC/solenoids/src/main/cpp/Robot.cpp
@@ -9,6 +9,9 @@
 #include <frc/Joystick.h>
 #include <frc/Solenoid.h>
 #include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+//#include <iostream>
+#include <wpi/raw_ostream.h>
 
 /**
  * This is a sample program showing the use of the solenoid classes during
@@ -32,25 +35,36 @@
  */
 class Robot : public frc::TimedRobot {
  public:
+  void RobotInit() override {
+    wpi::outs() << "hello from RobotInit\n";
+  }
+
+  void TeleopInit() override { }
+
   void TeleopPeriodic() override {
     /* The output of GetRawButton is true/false depending on whether the button
-     * is pressed; Set takes a boolean for for whether to use the default
+     * is pressed; Set takes a boolean for whether to use the default
      * (false) channel or the other (true).
      */
-    m_solenoid0.Set(m_stick.GetRawButton(kSolenoidButton1));
-    m_solenoid1.Set(m_stick.GetRawButton(kSolenoidButton2));
+    // m_solenoid4.Set(m_stick.GetRawButton(kSolenoidButton0));
+    // m_solenoid5.Set(m_stick.GetRawButton(kSolenoidButton1));
 
     /* In order to set the double solenoid, we will say that if neither button
      * is pressed, it is off, if just one button is pressed, set the solenoid to
      * correspond to that button, and if both are pressed, set the solenoid to
      * Forwards.
      */
+    wpi::outs() << "teleop loop";
+    frc::SmartDashboard::PutNumber ("DB/logtest", 1);
     if (m_stick.GetRawButton(kDoubleSolenoidForward)) {
       m_doubleSolenoid.Set(frc::DoubleSolenoid::kForward);
+      frc::SmartDashboard::PutString ("DB/solonoid", "forward");
     } else if (m_stick.GetRawButton(kDoubleSolenoidReverse)) {
       m_doubleSolenoid.Set(frc::DoubleSolenoid::kReverse);
+      frc::SmartDashboard::PutString ("DB/solonoid", "reverse");
     } else {
       m_doubleSolenoid.Set(frc::DoubleSolenoid::kOff);
+      frc::SmartDashboard::PutString ("DB/solonoid", "off");
     }
   }
 
@@ -60,16 +74,26 @@ class Robot : public frc::TimedRobot {
   // Solenoid corresponds to a single solenoid.
   frc::Solenoid m_solenoid0{0};
   frc::Solenoid m_solenoid1{1};
+  frc::Solenoid m_solenoid2{2};
+  frc::Solenoid m_solenoid3{3};
+  // frc::Solenoid m_solenoid4{4};
+  // frc::Solenoid m_solenoid5{5};
 
   // DoubleSolenoid corresponds to a double solenoid.
-  frc::DoubleSolenoid m_doubleSolenoid{2, 3};
+   frc::DoubleSolenoid m_doubleSolenoid{4,5};
 
-  static constexpr int kSolenoidButton1 = 1;
-  static constexpr int kSolenoidButton2 = 2;
-  static constexpr int kDoubleSolenoidForward = 3;
-  static constexpr int kDoubleSolenoidReverse = 4;
+  // joystick buttons
+  static constexpr int kSolenoidButton0 = 1;
+  static constexpr int kSolenoidButton1 = 2;
+  static constexpr int kSolenoidButton2 = 3;
+  static constexpr int kSolenoidButton3 = 4;
+  static constexpr int kSolenoidButton4 = 5;
+  static constexpr int kSolenoidButton5 = 6;
+   static constexpr int kDoubleSolenoidForward = 1;
+   static constexpr int kDoubleSolenoidReverse = 2;
 };
 
 #ifndef RUNNING_FRC_TESTS
-int main() { return frc::StartRobot<Robot>(); }
+int main() { 
+  return frc::StartRobot<Robot>(); }
 #endif
